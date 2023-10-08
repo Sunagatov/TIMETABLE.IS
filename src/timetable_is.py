e2d17244deeebe36d75ae8abdde5be17f12107a9
zufar-sunagatov <zufar.sunagatov@gmail.com>
2023-10-09 00:00:08 +0100

    timetable_is.py was added

--- src/timetable_is.py
+++ src/timetable_is.py
@@ -4,25 +4,23 @@ from timetable_is import Timetable
 # Create a Telegram bot
 bot = telegram.Bot(YOUR_BOT_TOKEN)
 
-# Define a function to get the timetable for a specific grade
-def get_timetable_for_grade(grade):
-    timetable = Timetable()
-    return timetable.get_timetable_for_grade(grade)
+class TimetableHandler:
+    def __init__(self):
+        self.timetable = Timetable()
 
-# Define a function to create a timetable
-def create_timetable(timetable_data):
-    timetable = Timetable()
-    timetable.create_timetable(timetable_data)
+    def get_timetable_for_grade(self, grade):
+        return self.timetable.get_timetable_for_grade(grade)
 
-# Define a function to edit a timetable
-def edit_timetable(timetable_id, timetable_data):
-    timetable = Timetable()
-    timetable.edit_timetable(timetable_id, timetable_data)
+    def create_timetable(self, timetable_data):
+        self.timetable.create_timetable(timetable_data)
 
-# Define a function to delete a timetable
-def delete_timetable(timetable_id):
-    timetable = Timetable()
-    timetable.delete_timetable(timetable_id)
+    def edit_timetable(self, timetable_id, timetable_data):
+        self.timetable.edit_timetable(timetable_id, timetable_data)
+
+    def delete_timetable(self, timetable_id):
+        self.timetable.delete_timetable(timetable_id)
+
+timetable_handler = TimetableHandler()
 
 # Define a command handler for the `/timetable` command
 @bot.message_handler(commands=['timetable'])
@@ -31,7 +29,7 @@ def timetable_command(message):
     grade = message.text.split()[1]
 
     # Get the timetable for the specified grade
-    timetable = get_timetable_for_grade(grade)
+    timetable = timetable_handler.get_timetable_for_grade(grade)
 
     # Send the timetable to the user
     bot.send_message(message.chat.id, timetable)
@@ -43,7 +41,7 @@ def create_timetable_command(message):
     timetable_data = message.text.split()[1:]
 
     # Create the timetable
-    create_timetable(timetable_data)
+    timetable_handler.create_timetable(timetable_data)
 
     # Send a confirmation message to the user
     bot.send_message(message.chat.id, "Timetable created successfully.")
@@ -55,7 +53,7 @@ def edit_timetable_command(message):
     timetable_id, timetable_data = message.text.split()[1:]
 
     # Edit the timetable
-    edit_timetable(timetable_id, timetable_data)
+    timetable_handler.edit_timetable(timetable_id, timetable_data)
 
     # Send a confirmation message to the user
     bot.send_message(message.chat.id, "Timetable edited successfully.")
@@ -67,7 +65,7 @@ def delete_timetable_command(message):
     timetable_id = message.text.split()[1]
 
     # Delete the timetable
-    delete_timetable(timetable_id)
+    timetable_handler.delete_timetable(timetable_id)
 
     # Send a confirmation message to the user
     bot.send_message(message.chat.id, "Timetable deleted successfully.")
